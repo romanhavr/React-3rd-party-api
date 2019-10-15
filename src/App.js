32f367b0e7e3baf569a4651abaae437a18eed63f
Roman Havryliv <romanhavr@ukr.net>
2019-10-15 09:55:29 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
+import Navigation from './components/Navigation';
+import Home from './components/Home';
+import About from './components/About';
+import YouViewer from './components/YouViewer';
+import DragDrop from './components/DragDrop';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <header className="App-header">
+          <Navigation />
+        </header>
+        <main>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/you-viewer">
+            <YouViewer />
+          </Route>
+          <Route path="/drag-drop">
+            <DragDrop />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+        </main>
+        <footer className="App-footer">
+          <h4>Created by <i>romanhavr</i></h4>
+        </footer>
+      </Router>
     </div>
   );
 }
